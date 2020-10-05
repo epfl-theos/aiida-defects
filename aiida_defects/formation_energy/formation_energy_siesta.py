31f8fe2b07849e290287857a5a4dcbc3790a5d96
Conrad Johnston <conrad.s.johnston@googlemail.com>
2020-10-05 20:48:55 +0100

    Convert testing environment to pytest
    Replace old tests with tests of workchains. Until the enhanced workflow testing tools become available, these tests serve to check if the workflows can be created without error.
    Adds new tests for: "FormationEnergyWorkchainQE", "GaussianCounterchargeWorkchain", "ModelPotentialWorkchain", and a placeholder for "FormationEnergyWorkchainSiesta".
    Also adds new placeholder workchain for the Siesta specific implementation of the formation energy workchain, FormationEnergyWorkchainSiesta.

---
+++ aiida_defects/formation_energy/formation_energy_siesta.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+########################################################################################
+# Copyright (c), The AiiDA-Defects authors. All rights reserved.                       #
+#                                                                                      #
+# AiiDA-Defects is hosted on GitHub at https://github.com/ConradJohnston/aiida-defects #
+# For further information on the license, see the LICENSE.txt file                     #
+########################################################################################
+from __future__ import absolute_import
+
+import numpy as np
+
+from aiida import orm
+from aiida.engine import WorkChain, calcfunction, ToContext, if_, submit
+from aiida.plugins import WorkflowFactory
+from aiida_quantumespresso.workflows.pw.base import PwBaseWorkChain
+
+from aiida_defects.formation_energy.formation_energy_base import FormationEnergyWorkchainBase
+from aiida_defects.formation_energy.utils import run_pw_calculation
+from .utils import get_raw_formation_energy, get_corrected_formation_energy, get_corrected_aligned_formation_energy
+
+
+class FormationEnergyWorkchainSiesta(FormationEnergyWorkchainBase):
+    """
+    Compute the formation energy for a given defect using Siesta
+    """
+    @classmethod
+    def define(cls, spec):
+        super(FormationEnergyWorkchainSiesta, cls).define(spec)
+
+        # Namespace to make it clear which code is being used.
+        spec.input_namespace('siesta.dft.supercell',
+            help="Inputs for DFT calculations on supercells")
+
+        # DFT inputs (Siesta)
+        # spec.input("siesta.dft.supercell.code",
+        #     valid_type=orm.Code,
+        #     help="The Siesta code to use for the supercell calculations")
+        # spec.input("siesta.dft.supercell.parameters",
+        #     valid_type=orm.Dict,
+        #     help="Parameters for the supercell calculations. Some will be set automatically")
+        # spec.input("siesta.dft.supercell.scheduler_options",
+        #     valid_type=orm.Dict,
+        #     help="Scheduler options for the Siesta calculation")
+
+        spec.outline(
+            cls.setup,
+            if_(cls.correction_required)(
+                if_(cls.is_gaussian_scheme)(
+                    cls.placeholder,
+                    cls.run_gaussian_correction_workchain),
+                if_(cls.is_point_scheme)(
+                    cls.raise_not_implemented
+                    #cls.prepare_point_correction_workchain,
+                    #cls.run_point_correction_workchain),
+                ),
+                cls.check_correction_workchain),
+            cls.compute_formation_energy
+        )
+
+    def placeholder(self):
+        """
+        Placeholder method
+        """
+        pass
---
+++ tests/workflows/formation_energy/corrections/gaussian_countercharge/model_potential/test_model_potential.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+########################################################################################
+# Copyright (c), The AiiDA-Defects authors. All rights reserved.                       #
+#                                                                                      #
+# AiiDA-Defects is hosted on GitHub at https://github.com/ConradJohnston/aiida-defects #
+# For further information on the license, see the LICENSE.txt file                     #
+########################################################################################
+"""Tests for the `ModelPotentialWorkchain` class."""
+import pytest
+from aiida.common import AttributeDict
+from aiida.orm import Float, List, Dict, Int, StructureData
+from aiida_defects.formation_energy.corrections.gaussian_countercharge.model_potential.model_potential import ModelPotentialWorkchain
+
+@pytest.fixture
+def generate_inputs_model_potential(generate_structure):
+    """Generate default inputs for `ModelPotentialWorkchain`"""
+
+    def _generate_inputs_model_potential():
+        """Generate default inputs for `ModelPotentialWorkchain`"""
+
+        inputs = {
+            'peak_charge': Float(1.0),
+            'defect_charge': Float(1.0),
+            'scale_factor': Int(2),
+            'host_structure': generate_structure(),
+            'defect_site': List(list=[0.5,0.5,0.5]),
+            'epsilon': Float(1.0),
+            'gaussian_params': List(list=[1.,1.,1.,1.,1.,1.,1.,1.,1.])
+        }
+
+        return inputs
+
+    return _generate_inputs_model_potential
+
+
+
+@pytest.fixture
+def generate_workchain_model_potential(generate_workchain, generate_inputs_model_potential):
+    """Generate an instance of a `ModelPotentialWorkchain`."""
+
+    def _generate_workchain_model_potential(exit_code=None):
+        entry_point = 'defects.formation_energy.corrections.gaussian_countercharge.model_potential'
+        inputs = generate_inputs_model_potential()
+        process = generate_workchain(entry_point, inputs)
+
+        if exit_code is not None:
+            node.set_process_state(ProcessState.FINISHED)
+            node.set_exit_status(exit_code.status)
+
+        return process
+
+    return _generate_workchain_model_potential
+
+def test_get_model_structure(aiida_profile, generate_workchain_model_potential):
+    """
+    Test `ModelPotentialWorkchain.get_model_structure`.
+    This checks that we can create the workchain successfully, and that model structure
+    is created correctly.
+    """
+    from numpy import ndarray
+
+    process = generate_workchain_model_potential()
+    process.get_model_structure()
+
+    assert isinstance(process.ctx.model_structure, StructureData)
+    assert isinstance(process.ctx.real_cell, ndarray)
+    assert isinstance(process.ctx.reciprocal_cell, ndarray)
+    assert isinstance(process.ctx.limits, List)
