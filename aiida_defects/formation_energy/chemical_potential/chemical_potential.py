7a3d962aa9967383d03c220d6ff11ecab8864bc1
Sokseiha <sokseiha.muy@gmail.com>
2020-12-15 14:37:10 +0100

    Reorganizing the workchain so that all the calculations are done by the calcfunctions in utils.py

--- aiida_defects/formation_energy/chemical_potential/chemical_potential.py
+++ aiida_defects/formation_energy/chemical_potential/chemical_potential.py
@@ -42,10 +42,9 @@ class ChemicalPotentialWorkchain(WorkChain):
 
         spec.outline(
             cls.setup,
-            cls.set_matrix_of_constraint,
-            cls.solve_matrix_of_constraint,
-            cls.get_centroid,
-            cls.chemical_potential,
+            cls.generate_matrix_of_constraints,
+            cls.solve_matrix_of_constraints,
+            cls.get_chemical_potential,
         )
         spec.output('stability_corners', valid_type=ArrayData)
         spec.output('matrix_of_constraints', valid_type=ArrayData)
@@ -73,7 +72,7 @@ class ChemicalPotentialWorkchain(WorkChain):
             N_species = len(composition)
         
         self.ctx.element_list = element_list
-        self.ctx.N_species = N_species
+        self.ctx.N_species = Int(N_species)
         formation_energy_dict = self.inputs.formation_energy_dict.get_dict()
         
         # check if the compound is stable or not. If not shift its energy down to put it on the convex hull and issue a warning.
@@ -84,10 +83,10 @@ class ChemicalPotentialWorkchain(WorkChain):
         
         self.ctx.formation_energy_dict = Dict(dict=formation_energy_dict)
 
-    def set_matrix_of_constraint(self):
-        compound_of_interest = Composition(self.inputs.compound.value)
-        N_competing_phases = len(self.ctx.formation_energy_dict.get_dict()) - 1
-        N_species = self.ctx.N_species
+    def generate_matrix_of_constraints(self):
+        #compound_of_interest = Composition(self.inputs.compound.value)
+        #N_competing_phases = len(self.ctx.formation_energy_dict.get_dict()) - 1
+        #N_species = self.ctx.N_species
 
         column_order = {} # To track which element corresponds to each column, the dependent element is always the last column
         i = 0
@@ -95,7 +94,7 @@ class ChemicalPotentialWorkchain(WorkChain):
             if ele.symbol != self.inputs.dependent_element.value:
                 column_order[ele.symbol] = i
                 i += 1
-        column_order[self.inputs.dependent_element.value] = self.ctx.N_species - 1
+        column_order[self.inputs.dependent_element.value] = self.ctx.N_species.value - 1
         self.ctx.column_order = Dict(dict=column_order)
         #self.report('Column order: {}'.format(column_order))
 
@@ -103,130 +102,41 @@ class ChemicalPotentialWorkchain(WorkChain):
         # Construct matrix containing all linear equations. The last column is the rhs 
         # of the system of equations
         ##############################################################################
-
-        # Construct the 1st equation corresponding to the compound of interest
-        eqns = np.zeros(N_species+1)
-        for ele in compound_of_interest:
-                eqns[column_order[ele.symbol]] = -1.0*compound_of_interest[ele]
-        eqns[N_species] = -1.0*self.ctx.formation_energy_dict.get_dict()[self.inputs.compound.value]
-        self.ctx.first_eqn = eqns
-        #self.report('The first equation is :{}'.format(eqns))
-
-        # Now loop through all the competing phases
-        for key in self.ctx.formation_energy_dict.keys():
-            # if key != compound:
-            if not same_composition(key, self.inputs.compound.value):
-                tmp = np.zeros(N_species+1)
-                temp_composition = Composition(key)
-                for ele in temp_composition:
-                    tmp[column_order[ele.symbol]] = temp_composition[ele]
-                tmp[N_species] = self.ctx.formation_energy_dict.get_dict()[key]
-                eqns = np.vstack((eqns, tmp))
-        #print(eqns)
-
-        # Add constraints corresponding to the stability with respect to decomposition into
-        # elements and combine it with the constraint on the stability of compound of interest
-        for ele in compound_of_interest:
-            if ele.symbol != self.inputs.dependent_element.value:
-                tmp = np.zeros(N_species+1)
-                tmp[column_order[ele.symbol]] = 1.0
-                eqns = np.vstack((eqns, tmp))
-                tmp = np.zeros(N_species+1)
-                tmp[column_order[ele.symbol]] = -1.0
-                tmp[N_species] = -1.*self.ctx.formation_energy_dict.get_dict()[self.inputs.compound.value]/compound_of_interest[ele]
-                eqns = np.vstack((eqns, tmp))
-        #print(eqns)
-
-        # Eliminate the dependent element (variable) from the equations
-        mask = eqns[1:, N_species-1] != 0.0
-        eqns_0 = eqns[1:,:][mask]
-        common_factor = compound_of_interest[self.inputs.dependent_element.value]/eqns_0[:, N_species-1]
-        eqns_0 = eqns_0*np.reshape(common_factor, (len(common_factor), 1)) # Use broadcasting
-        eqns_0 = (eqns_0+eqns[0,:])/np.reshape(common_factor, (len(common_factor), 1)) # Use broadcasting
-        eqns[1:,:][mask] = eqns_0
-        #print(eqns)
-
-        # Store the matrix of constraint (before removing the depedent-element column) in the database
-        constraints_with_dependent_element = ArrayData()
-        constraints_with_dependent_element.set_array('data', eqns)
-
-        #set_of_constraints = return_matrix_of_constraint(temp_data)
-        #self.ctx.constraints = set_of_constraints
-        #self.out('matrix_of_constraints', set_of_constraints)
-        #matrix = np.delete(eqns, N_species-1, axis=1)
-        #matrix_data = ArrayData()
-        #matrix_data.set_array('set_of_constraints', matrix)
-
-        # Removing column corresponding to the dependent element from the set of equations correponding to the constraints
-        # that delineate the stability region
-        matrix_data = remove_column_of_dependent_element(constraints_with_dependent_element, Int(N_species))
-        self.ctx.matrix_eqns = matrix_data
-        self.out('matrix_of_constraints', matrix_data)
-
-    def solve_matrix_of_constraint(self):
-        matrix_eqns = self.ctx.matrix_eqns.get_array('data')
-        #N_species = matrix_eqns.shape[1]
-
-        ### Look at all combination of lines and find their intersections
-        comb = combinations(np.arange(np.shape(matrix_eqns)[0]), self.ctx.N_species-1)
-        intersecting_points = []
-        for item in list(comb):
-            try:
-                point = np.linalg.solve(matrix_eqns[item,:-1], matrix_eqns[item,-1])    
-                intersecting_points.append(point)
-            except np.linalg.LinAlgError:
-                ### Singular matrix: lines are parallels therefore don't have any intersection
-                pass
         
-        ### Determine the points that form the 'corners' of stability region. These are intersecting point that verify all the constraints.
-        intersecting_points = np.array(intersecting_points)
-        get_constraint = np.dot(intersecting_points, matrix_eqns[:,:-1].T)
-        check_constraint = (get_constraint - np.reshape(matrix_eqns[:,-1] ,(1, matrix_eqns.shape[0]))) <= self.inputs.tolerance.value
-        bool_mask = [not(False in x) for x in check_constraint]
-        corners_of_stability_region = intersecting_points[bool_mask]
-        corners_of_stability_region = remove_duplicate(corners_of_stability_region)
-        
-        if corners_of_stability_region.size == 0:
-            self.report('The stability region cannot be determined. The compound {} is probably unstable'.format(self.inputs.compound.value)) 
-            return self.exit_codes.ERROR_CHEMICAL_POTENTIAL_FAILED
+        self.ctx.matrix_eqns = get_matrix_of_constraints(
+                                    self.ctx.N_species,
+                                    self.inputs.compound,
+                                    self.inputs.dependent_element,
+                                    self.ctx.column_order,
+                                    self.ctx.formation_energy_dict
+                                    )
+        self.out('matrix_of_constraints', self.ctx.matrix_eqns)
 
-        stability_data = ArrayData()
-        stability_data.set_array('data', corners_of_stability_region)
-        ordered_stability_corners = Order_point_clockwise(stability_data)
-        self.ctx.stability_corners = ordered_stability_corners
+    def solve_matrix_of_constraints(self):
+        self.ctx.stability_corners = get_stability_corners(
+                                        self.ctx.matrix_eqns, 
+                                        self.ctx.N_species, 
+                                        self.inputs.compound, 
+                                        self.inputs.tolerance
+                                        )
         #self.report('The stability corner is : {}'.format(ordered_stability_corners.get_array('data')))
-        self.out("stability_corners", ordered_stability_corners)
+        self.out("stability_corners", self.ctx.stability_corners)
 
-    def get_centroid(self):
-        '''
-        Use to determine centroid (as oppose to center). The center is defined as the average coordinates of the corners 
-        while a centroid is the average cooridinates of every point inside the polygone or polyhedron.
-        For binary compounds, the stability region is a one-dimensional segment. The centroid coincides with the center.
-        For ternary and quarternary compounds, the centroid is returned.
-        For quinternary compound and hight, the center is returned.
-        '''
-        stability_corners = self.ctx.stability_corners.get_array('data')
-        M = self.ctx.matrix_eqns.get_array('data')
-        #N_specie = M.shape[1]
-        if self.ctx.N_species == 2:
-            ctr_stability = np.mean(stability_corners, axis=0) #without the dependent element
-        else:
-            grid = get_grid(stability_corners, M)
-            ctr_stability = get_centroid(grid) #without the dependent element
-        
-        ### Add the corresponding chemical potential of the dependent element
-        with_dependent = (self.ctx.first_eqn[-1]-np.sum(ctr_stability*self.ctx.first_eqn[:-2]))/self.ctx.first_eqn[-2]
-        centroid_of_stability = np.append(ctr_stability, with_dependent)
-        self.report('Centroid of the stability region is {}'.format(centroid_of_stability))
+    def get_chemical_potential(self):
+        centroid = get_center_of_stability(
+                        self.inputs.compound, 
+                        self.inputs.dependent_element, 
+                        self.ctx.stability_corners, 
+                        self.ctx.N_species, 
+                        self.ctx.matrix_eqns
+                        )
+        self.report('Centroid of the stability region is {}'.format(centroid.get_array('data')))
 
-        ctrd = ArrayData()
-        ctrd.set_array('data', centroid_of_stability)
-        self.ctx.centroid = ctrd
-
-    def chemical_potential(self):
-        chem_ref = self.inputs.ref_energy.get_dict()
-        chemical_potential = get_chemical_potential(self.ctx.centroid, self.inputs.ref_energy, self.ctx.column_order)
-        self.ctx.chemical_potential = chemical_potential
-        self.out('chemical_potential', chemical_potential)
-        self.report('The chemical potential is {}'.format(str(chemical_potential.get_dict())))
+        self.ctx.chemical_potential = get_chemical_potential(
+                                            centroid, 
+                                            self.inputs.ref_energy, 
+                                            self.ctx.column_order
+                                            )
+        self.out('chemical_potential', self.ctx.chemical_potential)
+        self.report('The chemical potential is {}'.format(self.ctx.chemical_potential.get_dict()))
 
